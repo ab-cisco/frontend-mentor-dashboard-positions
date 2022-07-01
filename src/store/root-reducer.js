b16b50477a0c7906c2ef5d55c011f483b0f1f63e
Aleksandr Berezin <alexbn@Aleksandrs-MacBook-Pro.local>
2022-07-02 00:03:54 +0300

    Create filters reducer. Added actions and selectors.

--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,9 +1,11 @@
 import { combineReducers } from "redux";
 
 import { positionReducer } from "./positions/position-reducer";
+import { filterReducer } from "./filters/filter-reducer";
 
 const rootReducer = combineReducers({
     positions: positionReducer,
+    filters: filterReducer,
 });
 
 export { rootReducer };
